3147feeb66c156005329747d78679fe445e613f1
Amanda Ackerman <ackerm44@gmail.com>
2017-10-16 21:26:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  counter = 1
+  counter = 0
   board.each do |cell|
     if cell == "X" || cell == "O"
       counter += 1
@@ -9,7 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 1
+  if turn_count(board) % 2 == 0
     return "X"
   else
     return "O"
